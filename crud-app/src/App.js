bd674fe2f0a0db1bfd5106cfc1498dd3e6d27303
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-16 19:50:13 +0200

    edit button

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -8,16 +8,44 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: " ",
+    type: " ",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    if (!name) {
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      showAlert(true, "success", "item added to the list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName(" ");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
   };
   return (
     <div className="App">
       <section className="section-center">
         <form className="grocery-form" onSubmit={handleSubmit}>
-          {alert.show && <Alert />}
+          {alert.show && (
+            <Alert {...alert} removeAlert={showAlert} list={list} />
+          )}
           <h3>grocery bud</h3>
           <div className="form-control">
             <input
@@ -31,10 +59,14 @@ function App() {
             </button>
           </div>
         </form>
-        <div className="grocery-container">
-          <List />
-          <button className="clear-btn">clear items</button>
-        </div>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} removeItem={removeItem} />
+            <button className="clear-btn" onClick={clearList}>
+              clear items
+            </button>
+          </div>
+        )}
       </section>
     </div>
   );
--- crud-app/src/List.js
+++ crud-app/src/List.js
@@ -1,7 +1,31 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = () => {
-  return <p>list</p>;
+import "./App.css";
+const List = ({ items, removeItem }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button
+                type="button"
+                className="trash-btn"
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default List;
