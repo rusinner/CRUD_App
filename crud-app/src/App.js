f89581c7368140bb4a80ed7e2852a6319f1db966
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-16 16:28:39 +0200

    4:03:40

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,7 +1,43 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 import "./App.css";
 
 function App() {
-  return <section className="App"></section>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+  return (
+    <div className="App">
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && <Alert />}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Your Item Here..."
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        <div className="grocery-container">
+          <List />
+          <button className="clear-btn">clear items</button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- crud-app/src/List.js
+++ crud-app/src/List.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 const List = () => {
-  return <h2>list component</h2>;
+  return <p>list</p>;
 };
 
 export default List;
