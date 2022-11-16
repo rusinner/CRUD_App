8392d84a549626cf2a71919d9df26200b88ddb1b
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-16 20:00:01 +0200

    local storage

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -18,7 +18,18 @@ function App() {
     if (!name) {
       showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
-      //deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
     } else {
       showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
@@ -38,6 +49,12 @@ function App() {
   const removeItem = (id) => {
     showAlert(true, "danger", "item removed");
     setList(list.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
   };
   return (
     <div className="App">
@@ -61,7 +78,7 @@ function App() {
         </form>
         {list.length > 0 && (
           <div className="grocery-container">
-            <List items={list} removeItem={removeItem} />
+            <List items={list} removeItem={removeItem} editItem={editItem} />
             <button className="clear-btn" onClick={clearList}>
               clear items
             </button>
